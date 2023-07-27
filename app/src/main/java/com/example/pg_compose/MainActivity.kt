b94eaf3eca0143d2d98ecbd731fdcf1508a86ed4
ostenda <ostenda95@gmail.com>
2023-07-27 21:32:49 +0100

    Rows, Columns, Basic Settings

--- app/src/main/java/com/example/pg_compose/MainActivity.kt
+++ app/src/main/java/com/example/pg_compose/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.pg_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pg_compose.ui.theme.PG_ComposeTheme
 
@@ -16,20 +23,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Michal")
+            Row(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxSize(0.5F),
+
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                Text("Hello")
+                Text("World")
+                Text("bottom")
+            }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PG_ComposeTheme {
-        Greeting("Android")
-    }
-}
